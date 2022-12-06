f1a4ec373d2d4c01083163b89de53fc9bf87ff43
Logan Mondal Bhamidipaty <76822456+FlyingWorkshop@users.noreply.github.com>
2022-12-05 16:48:37 -0800

    Add files via upload

---
+++ koopman/dynamics.py
@@ -0,0 +1,57 @@
+import numpy as np
+from numpy.random import default_rng
+from scipy.linalg import expm
+
+
+RNG = default_rng()
+TIMESTEPS_PER_TRAJECTORY = 50
+TIME = np.linspace(0, 1, TIMESTEPS_PER_TRAJECTORY)
+
+
+class LDS:
+    def __init__(self, A):
+        """
+        Linear first order system of homogeneous equations--i.e., a system of the form x' = Ax
+        :param A: real-valued square coeffecient matrix
+        """
+        self.A = np.array(A, dtype=np.float64)
+        self.dim = self.A.shape[0]
+
+    def make_rand_trajs(self, low, high, num_trajs, noise_var=0.1):
+        init_conds = RNG.integers(low, high, size=(num_trajs, self.dim))
+        trajs = self._calc_trajs(self.A, init_conds, TIME)
+        if noise_var:
+            noisy_trajs = self._calc_noisy_trajs(self.A, init_conds, TIME, noise_var)
+            return trajs, noisy_trajs
+        else:
+            return trajs
+
+    @staticmethod
+    def _calc_trajs(A, init_conds, time=TIME):
+        matrix_exponentials = np.array([expm(A * t) for t in time])
+        trajs = np.einsum('ijk,lk->lij', matrix_exponentials, init_conds)  # shape = (num trajs, num steps, dim)
+        return trajs
+
+    @staticmethod
+    def _calc_noisy_trajs(A, init_conds, time=TIME, noise_var=0.1):
+        """
+        Generate noisy trajectories. Noise is added DURING trajectory generation rather than as a post-processing step.
+        """
+        dt = time[1] - time[0]
+        matrix_exponential = expm(A * dt)
+        noisy_trajs = []
+        for x0 in init_conds:
+            traj = [x0]
+            for _ in time[1:]:
+                noise = RNG.normal(0, noise_var ** 2, size=(x0.shape))
+                traj.append(matrix_exponential @ traj[-1] + noise)
+            noisy_trajs.append(traj)
+        noisy_trajs = np.array(noisy_trajs)
+        return noisy_trajs
+
+
+class RandomLDS(LDS):
+    def __init__(self, dim: int, low=-5, high=5):
+        A = RNG.integers(low, high, size=(dim, dim))
+        super().__init__(A)
+
---
+++ koopman/utils.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.collections import LineCollection
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
+from sklearn.decomposition import PCA
+
+
+def _plot2d(trajs_grid, labels: list[str] = None):
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    for i, trajs in enumerate(trajs_grid):
+        lines = LineCollection(trajs, color=f"C{i}", label=labels[i])
+        ax.add_collection(lines)
+    minima = trajs_grid.min(axis=(0, 1, 2))
+    maxima = trajs_grid.max(axis=(0, 1, 2))
+    ax.set_xlim(minima[0], maxima[0])
+    ax.set_ylim(minima[1], maxima[1])
+    ax.legend()
+    plt.show()
+
+
+def _plot3d(trajs_grid, labels: list[str] = None):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    for i, trajs in enumerate(trajs_grid):
+        lines = Line3DCollection(trajs, color=f"C{i}", label=labels[i])
+        ax.add_collection(lines)
+    minima = trajs_grid.min(axis=(0, 1, 2))
+    maxima = trajs_grid.max(axis=(0, 1, 2))
+    ax.set_xlim(minima[0], maxima[0])
+    ax.set_ylim(minima[1], maxima[1])
+    ax.set_zlim(minima[2], maxima[2])
+    ax.legend()
+    plt.show()
+
+
+def _plot4d(trajs_grid, pca: PCA, labels: list[str] = None):
+    assert pca.n_components == 2 or pca.n_components == 3
+    trajs_grid = _apply_pca_to_grid(trajs_grid, pca)
+    if pca.n_components == 2:
+        _plot2d(trajs_grid, labels)
+    else:
+        _plot3d(trajs_grid, labels)
+
+
+def make_pca(trajs: np.ndarray, n_components=3):
+    dim = trajs.shape[-1]
+    X = trajs.reshape((-1, dim))
+    pca = PCA(n_components=n_components)
+    pca.fit(X)
+    return pca
+
+
+def _apply_pca_to_grid(trajs_grid, pca) -> np.ndarray:
+    dim = trajs_grid.shape[-1]
+    X = trajs_grid.reshape((-1, dim))
+    result = pca.transform(X)
+    target_shape = list(trajs_grid.shape)
+    target_shape[-1] = pca.n_components
+    result = result.reshape(target_shape)
+    return result
+
+
+def plot(trajs_grid: list[np.ndarray], target_dim=3, pca=None, max_lines=30, labels: list[str] = None):
+    trajs_grid = np.array([elem[:max_lines] for elem in trajs_grid])
+    trajs_grid = trajs_grid[..., :max_lines]
+    dim = trajs_grid.shape[-1]
+    if dim == 2:
+        _plot2d(trajs_grid, labels)
+    elif dim == 3:
+        _plot3d(trajs_grid, labels)
+    else:
+        if pca is None:
+            pca = make_pca(trajs_grid[0], n_components=target_dim)
+        _plot4d(trajs_grid, pca, labels)
